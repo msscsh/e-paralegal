0555c53374cb50c655b5830a8109f40a5e13d901
msscsh <itsbruno42@gmail.com>
2023-12-23 19:23:11 -0300

    adicionando extração de lista de processos no arquivo

--- src/consultas/tst_num_unica/executor.py
+++ src/consultas/tst_num_unica/executor.py
@@ -5,28 +5,44 @@ from selenium.webdriver.common.by import By
 
 from webdriver_manager.chrome import ChromeDriverManager
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+import pandas as pd
 
-url = f'https://consultaprocessual.tst.jus.br/consultaProcessual/consultaTstNumUnica.do?'\
-'consulta=Consultar&'\
-'conscsjt=&'\
-'numeroTst=0000603&'\
-'digitoTst=86&'\
-'anoTst=2019&'\
-'orgaoTst=5&'\
-'tribunalTst=17&'\
-'varaTst=0009&'\
-'submit=Consultar'
+def get_data_ultima_movimentacao(numero_processo):
+	options = Options()
+	options.add_argument('--headless')
+	options.add_argument('--no-sandbox')
+	options.add_argument('--disable-dev-shm-usage')
+	driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.get(url)
-print(driver.title)
+	url = f'https://consultaprocessual.tst.jus.br/consultaProcessual/consultaTstNumUnica.do?'\
+	'consulta=Consultar&'\
+	'conscsjt=&'\
+	'numeroTst=0000603&'\
+	'digitoTst=86&'\
+	'anoTst=2019&'\
+	'orgaoTst=5&'\
+	'tribunalTst=17&'\
+	'varaTst=0009&'\
+	'submit=Consultar'
 
-celulas = driver.find_elements(By.CLASS_NAME, 'historicoProcesso')
-print(celulas[0].text)
+	driver.get(url)
+	print(driver.title)
 
-driver.close()
-driver.quit()
+	celulas = driver.find_elements(By.CLASS_NAME, 'historicoProcesso')
+	return celulas[1].text
+
+	driver.close()
+	driver.quit()
+
+def recuperar_processos_no_arquivo():
+	caminho_do_arquivo = './src/consultas/tst_num_unica/planilha.xlsx'
+	df = pd.read_excel(caminho_do_arquivo, header=None, skiprows=4)
+	if 0 in df.columns:
+		return df[1].tolist()
+
+lista_processos = recuperar_processos_no_arquivo()
+print(len(lista_processos))
+
+numero_processo = ''
+data_ultima_movimentacao = get_data_ultima_movimentacao(numero_processo)
+print(data_ultima_movimentacao)
