7c01cb1a9a890910c6612a717e08780f574b04fe
msscsh <itsbruno42@gmail.com>
2023-12-23 22:10:39 -0300

    adicionando tratamento de erro quando bot

--- src/consultas/tst_num_unica/executor.py
+++ src/consultas/tst_num_unica/executor.py
@@ -15,32 +15,38 @@ from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
 
 def get_dados_da_ultima_movimentacao(numero_processo):
-	options = Options()
-	options.add_argument('--headless')
-	options.add_argument('--no-sandbox')
-	options.add_argument('--disable-dev-shm-usage')
-	driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-	parte_do_numero_do_processo = numero_processo.split('.')
-
-	url = f'https://consultaprocessual.tst.jus.br/consultaProcessual/consultaTstNumUnica.do?'\
-	'consulta=Consultar&'\
-	'conscsjt=&'\
-	f'numeroTst={parte_do_numero_do_processo[0].split("-")[0]}&'\
-	f'digitoTst={parte_do_numero_do_processo[0].split("-")[1]}&'\
-	f'anoTst={parte_do_numero_do_processo[1]}&'\
-	f'orgaoTst={parte_do_numero_do_processo[2]}&'\
-	f'tribunalTst={parte_do_numero_do_processo[3]}&'\
-	f'varaTst={parte_do_numero_do_processo[4]}&'\
-	'submit=Consultar'
-
-	driver.get(url)
-	time.sleep(1)
-	celulas = driver.find_elements(By.CLASS_NAME, 'historicoProcesso')
 
 	dados_ultima_movimentacao = []
-	dados_ultima_movimentacao.append(celulas[1].text)
-	dados_ultima_movimentacao.append(celulas[2].text)
+
+	try:
+		options = Options()
+		options.add_argument('--headless')
+		options.add_argument('--no-sandbox')
+		options.add_argument('--disable-dev-shm-usage')
+		driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+		parte_do_numero_do_processo = numero_processo.split('.')
+
+		url = f'https://consultaprocessual.tst.jus.br/consultaProcessual/consultaTstNumUnica.do?'\
+		'consulta=Consultar&'\
+		'conscsjt=&'\
+		f'numeroTst={parte_do_numero_do_processo[0].split("-")[0]}&'\
+		f'digitoTst={parte_do_numero_do_processo[0].split("-")[1]}&'\
+		f'anoTst={parte_do_numero_do_processo[1]}&'\
+		f'orgaoTst={parte_do_numero_do_processo[2]}&'\
+		f'tribunalTst={parte_do_numero_do_processo[3]}&'\
+		f'varaTst={parte_do_numero_do_processo[4]}&'\
+		'submit=Consultar'
+
+		driver.get(url)
+		time.sleep(2)
+		celulas = driver.find_elements(By.CLASS_NAME, 'historicoProcesso')
+		dados_ultima_movimentacao.append(celulas[1].text)
+		dados_ultima_movimentacao.append(celulas[2].text)
+	except Exception  as e:
+		print(f'Erro: {e}')
+	finally:
+		driver.quit()
 
 	return dados_ultima_movimentacao
 
@@ -65,13 +71,21 @@ df = criar_dataframe_da_planilha()
 lista_processos = recuperar_processos_do_dataframe(df)
 dados_data = []
 dados_fase = []
+index = 0
 
-for numero_processo in lista_processos:
+while index < len(lista_processos):
+	numero_processo = lista_processos[index]
+
 	if len(numero_processo) == 25:
-		print(numero_processo)
+		print(f'Index: {index} Processo: {numero_processo}')
 		dados_ultima_movimentacao = get_dados_da_ultima_movimentacao(numero_processo)
-		dados_data.append(dados_ultima_movimentacao[0])
-		dados_fase.append(dados_ultima_movimentacao[1])
+
+		if len(dados_ultima_movimentacao) > 0:
+			index += 1
+			dados_data.append(dados_ultima_movimentacao[0])
+			dados_fase.append(dados_ultima_movimentacao[1])
+		else:
+			time.sleep(5)
 	else:
 		print(f'Numero de processo inválido: {numero_processo}')
 
